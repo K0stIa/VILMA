67c5a6b7dc0499654b640e34f38863a86a9cae27
Kostia <antonjuk@gmail.com>
2015-08-14 01:37:54 +0200

    up main

--- main.cpp
+++ main.cpp
@@ -304,12 +304,25 @@ void RunExperimentTheta(const string &input_dir, const string &output_filename,
       std::chrono::steady_clock::now();
 
   // train W weights
-  vector<double> opt_w = mae_oracle.learn();
+  // vector<double> opt_w = mae_oracle.learn();
 
-  const int num_gender = (int)opt_w.size() / data.x->kCols;
+  const int num_gender = 1;  //(int)opt_w.size() / data.x->kCols;
+
+  const string param_path =
+      "/Users/Shared/research/code/python/jmlr/sparse_sgd/sparse_sgd/data/"
+      "weights.bin";
+
+  std::ifstream file(param_path, std::ios::in | std::ios::binary);
+  if (!file) {
+    cout << "File not found\n";
+    return;
+  }
 
   // project data
-  DenseVecD W((int)opt_w.size(), &opt_w[0]);
+  DenseVecD W2(&file);
+
+  file.close();
+  DenseVecD W(723712, W2.data_);
   vector<double> wx(data.x->kRows * num_gender);
   BmrmOracle::SingleGenderNoBetaBmrmOracle<Vilma::MAELoss>::ProjectData(
       W, &data, &wx[0]);
@@ -319,6 +332,12 @@ void RunExperimentTheta(const string &input_dir, const string &output_filename,
   DenseVecD theta((data.ny - 1) * num_gender);
   BmrmOracle::SingleGenderNoThetaExpBmrmOracle<Vilma::MAELoss>::TrainTheta(
       &theta, &data, &wx[0]);
+
+  //  DenseVecD beta((data.ny) * num_gender);
+  //  BmrmOracle::SingleGenderNoBetaBmrmOracle<Vilma::MAELoss>::TrainBeta(
+  //      &beta, &data, &wx[0]);
+  //
+  //  for (int i = 0; i < data.ny - 1; ++i) theta[i] = beta[i] - beta[i + 1];
 
   // print learning time
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -335,6 +354,16 @@ void RunExperimentTheta(const string &input_dir, const string &output_filename,
     opt_params[W.dim_ + i] = theta[i];
     assert(W.dim_ + i < (int)opt_params.size());
   }
+
+  //  // glue W and beta to single vector
+  //  vector<double> opt_params(W.dim_ + beta.dim_);
+  //
+  //  for (int i = 0; i < W.dim_; ++i) opt_params[i] = W[i];
+  //
+  //  for (int i = 0; i < beta.dim_; ++i) {
+  //    opt_params[W.dim_ + i] = beta[i];
+  //    assert(W.dim_ + i < (int)opt_params.size());
+  //  }
 
   //  // save weights
   //  {
@@ -359,10 +388,11 @@ void RunExperimentTheta(const string &input_dir, const string &output_filename,
   LoadData(input_dir + "-val.bin", &val_data, int(1e9), int(1e9));
   std::cout << "Validation data loaded\n";
 
-  BmrmOracle::SingleGenderNoThetaExpBmrmOracle<Vilma::MAELoss> oracle(&data);
+  BmrmOracle::SingleGenderNoThetaExpBmrmOracle<Vilma::MAELoss> exp_oracle(
+      &data);
 
   // compute validation error
-  double val_error = oracle.EvaluateModel(&val_data, &opt_params[0]);
+  double val_error = exp_oracle.EvaluateModel(&val_data, &opt_params[0]);
   std::cout << "val error: " << val_error << std::endl;
 
   Data tst_data;
@@ -373,7 +403,7 @@ void RunExperimentTheta(const string &input_dir, const string &output_filename,
   std::cout << "Validation data loaded\n";
 
   // compute validation error
-  double tst_error = oracle.EvaluateModel(&tst_data, &opt_params[0]);
+  double tst_error = exp_oracle.EvaluateModel(&tst_data, &opt_params[0]);
   std::cout << "tst error: " << tst_error << std::endl;
 
   //  // save errors
@@ -484,7 +514,7 @@ int main(int argc, const char *argv[]) {
 
   oracle_name = oracle_name.substr(0, oracle_name.find('-'));
 
-  if (true) {
+  if (0) {
     RunExperimentTheta(input_dir, output_dir, lambda, supervised, fraction);
   } else if (oracle_name == "SingleGenderNoBetaBmrmOracle") {
     //
