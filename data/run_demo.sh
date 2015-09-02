8aa9e7406ee643a29b374d6b09cf7a8073c32317
Kostia <antonjuk@gmail.com>
2015-09-02 15:55:16 +0200

    no message

---
+++ data/run_demo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+rm -r demo
+mkdir demo
+
+cd demo
+
+featdim=723712
+nclasees=55
+lambda=1.0
+bmrm_cp_buffer_size=300
+supervised=1000
+
+for experiment_id in 0 1000 2000 3000 4000
+do
+  total=$(($supervised+$experiment_id))
+  rm $(pwd)/$total/*
+  mkdir $(pwd)/$total
+  cd $(pwd)/$total
+  cat ../../morph_features.txt | sed -n 1,${total}p > morph_features.txt
+  cat ../../morph_supervised_labeling.txt | sed -n 1,${supervised}p > morph_partial_labeling.txt
+  offset=$(($supervised+1))
+  cat ../../morph_partial_labeling.txt | sed -n ${offset},${total}p >> morph_partial_labeling.txt
+  cat ../../morph_supervised_labeling.txt | sed -n 1,${total}p > morph_supervised_labeling.txt
+
+  ./../../../build/train_vilma $(pwd)/morph_features.txt $(pwd)/morph_partial_labeling.txt $(pwd)/model.bin ${featdim} ${nclasees} ${lambda} ${bmrm_cp_buffer_size}
+
+  ./../../../build/test_vilma $(pwd)/morph_features.txt $(pwd)/morph_supervised_labeling.txt $(pwd)/model.bin ${featdim} ${nclasees} > error.txt
+ 
+  cd ..
+done
+
+cd ..
+# for file in "morph_features.txt" "morph_partial_labeling.txt" "morph_supervised_labeling.txt"
+#do
+#  extension="${file##*.}"
+#  filename="${file%.*}"
+#  cat ../${filename}.${extension} | sed -n 1,1000p > demo/${filename}_short.${extension}
+#done
