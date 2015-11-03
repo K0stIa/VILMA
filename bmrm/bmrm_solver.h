35ce5e5ef21daf7ab1ecf25132e4d314aee918af
Kostia <antonjuk@gmail.com>
2015-11-03 11:23:45 +0100

    added copyright for bmrm solver

--- bmrm/bmrm_solver.h
+++ bmrm/bmrm_solver.h
@@ -1,3 +1,15 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Michal Uricar
+ * Written (W) 2015 Kostiantyn Antoniuk
+ * Copyright (C) 2012 Michal Uricar
+ * Copyright (C) 2015 Kostiantyn Antoniuk
+ */
+ 
 #ifndef _BMRM_SOLVER
 #define _BMRM_SOLVER
 
