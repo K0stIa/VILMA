01a8c621b528774da08563b01828157edda96010
Kostia <kostia@MacBook-Pro-K0stIa.local>
2015-11-10 23:20:51 +0100

    fix mac issues

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
+OBOE=$(pwd)/OBOE/dist
+CXX_COMPILER=g++-4.7
+
 rm -r build
 mkdir build
 cd build
-local_host=`uname -s`
-echo "local host: $local_host"
 
-CXX_COMPILER=g++-4.7
 
 #if  [['$local_host' -eq 'Darwin']]; then
 #echo "Running on mac os"
- cmake -DOBOE=/Users/Shared/research/code/python/jmlr/oboe -DLAPACKPP=/Users/Shared/research/code/python/jmlr/lapackpp ..
+cmake -DOBOE=${OBOE} -DLAPACKPP=${OBOE}/lapackpp ..
 #else
 #echo "Running on cmpgrid"
 #cmake -DOBOE=/home.dokt/antonkos/lib/oboe -DLAPACKPP=/home.dokt/antonkos/lib/lapackpp -DCMAKE_CXX_COMPILER=${CXX_COMPILER} ..
--- mvilma_mex.cc
+++ mvilma_mex.cc
@@ -1,6 +1,71 @@
 #include <cstdio>
 #include <cstring>
 #include "mex.h"
+#include "data.h"
+#include "sparse_vector.h"
+#include "sparse_matrix.h"
+
+
+bool BuildVilmaData(mxArray *sparse_mat, Data *data) {
+  if (data == nullptr) return false;
+
+  mwSize m_ = mxGetM(sparse_mat);
+  mwSize n_ = mxGetN(sparse_mat);
+  mwSize nz = mxGetNzmax(sparse_mat);
+  double *sr = mxGetPr(sparse_mat);
+  mwIndex *irs = mxGetIr(sparse_mat);
+  mwIndex *jcs = mxGetJc(sparse_mat);
+
+  for (mwIndex i = 0; i < nz; ++i) {
+    mexPrintf("x: %d, y: %d, V %f\n", int(irs[i]), int(jcs[i]), sr[i]);
+  }
+
+  const int dim = static_cast<int>(m_);
+  const int n = static_cast<int>(n_);
+
+  std::cout << "File has " << n << " examples with dim=" << dim << std::endl;
+  std::cout << " Vilma will use " << max_num_examples << " with "
+            << num_supervised_examples << " supervised examples" << std::endl;
+
+  data->x = new Vilma::SparseMatrix<double>(n, dim);
+
+  for (int i = 0; i < n; ++i) {
+    mwIndex begin = jcs[i];
+    mwIndex end = jcs[i + 1];
+    int non_zero = end - begin;
+    assert(non_zero > 0);
+    int *index = new int[non_zero];
+    double *vals = new double[non_zero];
+
+    for (int j = 0; j < non_zero; ++j) {
+      index[j] = static_cast<int>(irc[j + begin]);
+      vals[j] = sr[j + begin];
+    }
+
+    // put example to the file
+    Vilma::SparseVector<double> *row = data->x->GetRow(i);
+    row->AssignNonZeros(index, vals, non_zero, dim);
+  }
+
+  if (data->x->IsCorrupted()) {
+    std::cout << "Data is Corrupted!\n";
+    assert(false);
+  }
+
+  data->yl = nullptr;
+  data->yr = nullptr;
+  data->y = nullptr;
+  data->z = nullptr;
+
+//      new Vilma::DenseVector<int>(static_cast<int>(upper_labeling.size()));
+//  for (int i = 0; i < (int)upper_labeling.size(); ++i) {
+//    data->y->data_[i] = upper_labeling[i];
+//  }
+  data->ny = -1;
+  data->nz = -1;
+
+  return true;
+}
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   // Get the command string
@@ -36,7 +101,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     // prhs[3] -- labels
     // prhs[4] -- n_classes
     // prhs[5] -- lambda
-    
+
     const int n_classes = mxGetScalar(prhs[4]);
     mexPrintf("n_classes: %d \n", n_classes);
 
